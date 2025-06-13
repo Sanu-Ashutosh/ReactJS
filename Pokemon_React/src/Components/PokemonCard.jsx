5b09395d4ea77131257a177c20fee3c25da716b2
Ashutosh Pradhan <sanu.ashutoshpradhan@gmail.com>
2025-06-13 10:57:58 +0530

    Update PokemonCard.jsx
    check

--- Pokemon_React/src/Components/PokemonCard.jsx
+++ Pokemon_React/src/Components/PokemonCard.jsx
@@ -4,15 +4,7 @@ let PokemonCard =()=>{
     <>
         <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptatibus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
         <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        
     </>
     );
 }

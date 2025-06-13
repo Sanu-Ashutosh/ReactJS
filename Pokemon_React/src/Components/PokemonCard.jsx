f159d694de53b25afbf4a128013b05fcf9065955
Ashutosh Pradhan <sanu.ashutoshpradhan@gmail.com>
2025-06-13 10:56:42 +0530

    `Added multiple h1 elements to PokemonCard component`

--- Pokemon_React/src/Components/PokemonCard.jsx
+++ Pokemon_React/src/Components/PokemonCard.jsx
@@ -2,7 +2,17 @@
 let PokemonCard =()=>{
     return (
     <>
-        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptatibus?</h1>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptatibus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid!</h1>
     </>
     );
 }
